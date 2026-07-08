57c4d5294c29cda61fcc4f5e4e93746ec6d8df36
renatoagostinhoandrade-beep <renato.agostinho.andrade@escola.pr.gov.br>
2026-07-08 19:58:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function aumentarTexto() {document.getElementById("mensagem").style.fontSize = "30px";}
+
+function mudarFonte() {document.getElementById("mensagem").style.fontFamily = "Courier New";}
+
+function negritoTexto() {document.getElementById("mensagem").style.fontWeight = "bold";}
