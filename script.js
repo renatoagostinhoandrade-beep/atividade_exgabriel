736f4f23afb6bc9862f07f543748e2431ff40d55
renatoagostinhoandrade-beep <renato.agostinho.andrade@escola.pr.gov.br>
2026-07-08 19:49:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function modoClaro() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+
+document.getElementById("mensagem").innerText= "e uma mente saudavel mora dentro de um corpo saudavel";
+
+}
+
+function modoEscuro() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+
+    document.getElementById("mensagem").innerText= "feche os olhos";}
