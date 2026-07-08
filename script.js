c623412b975ca8bb08279006fd987ceba4b65267
renatoagostinhoandrade-beep <renato.agostinho.andrade@escola.pr.gov.br>
2026-07-08 20:26:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function pegarValores() {
+    let n1 = Number(document.getElementById("num1").value);
+    let n2 = Number(document.getElementById("num2").value);
+    return {n1, n2};
+}
+
+function somar() {
+    let valores = pegarValores();
+    document.getElementById("resultado").innerText = "Resultado: " + (valores.n1 + valores.n2);}
+
+    function subtrair() {
+        let valores = pegarValores();
+        document.getElementById("resultado").innerText = "Resultado: " + (valores.n1 - valores.n2);}
+
+
+        function multiplicar() {
+            let valores = pegarValores();
+            document.getElementById("resultado").innerText = "Resultado: " + (valores.n1 * valores.n2);
+        }
+
+        function dividir() {
+            let valores = pegarValores();
+            if (valores.n2 === 0) {
+                document.getElementById("resultado").innerText = "error";}
+
+                else {
+                    document.getElementById("resultado").innerText = "Resultado: " + (valores.n1 / valores.n2);
+                }}
